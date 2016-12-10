a4440ec2db6ea6cb01be0a58de7c035f38ce6d07
Suranji Kannangara <Suranji Kannangara>
2016-12-11 03:39:27 +0530

    Initial commit

---
+++ app/src/main/java/com/sut/mylistdemo/MainActivity.java
@@ -0,0 +1,13 @@
+package com.sut.mylistdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
